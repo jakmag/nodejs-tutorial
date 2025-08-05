3bb8bc0b3b3e05bca435a850b6b9b410aaeefd31
jakmag <magnes@brightpick.ai>
2025-08-05 16:58:08 +0200

    Second Commit

--- server.js
+++ server.js
@@ -11,10 +11,96 @@ const myEmitter = new Emitter();
 
 const PORT = process.env.PORT || 3500;
 
-const server = http.createServer((req,res) => {
-    console.log(req.url, req.method);
 
+const serveFile = async (filePath, contentType, response) => {
+    try {
+        const rawData = await fsPromises.readFile(filePath,'utf8');
+        const data = contentType === 'application/json' ? JSON.parse(rawData) : rawData;
+        response.writeHead(200,{'Content-Type': contentType});
+        response.end(
+            contentType === 'application/json' ? JSON.stringify(data) : data
+        );
+    } catch (error) {
+        console.log(error);
+        response.statusCode = 500;    
+        response.end();
+    }
+}
+
+
+// determine filepath and content type
+
+const server = http.createServer((req,res) => { 
+
+    console.log(`Method: ${req.method} , URL: ${req.url}`);
+
+    const extension = path.extname(req.url);
+
+
+    console.log(`Extention: ${extension}`);
     
+
+    let contentType; 
+
+    switch (extension) {
+        case '.css':
+            contentType = 'text/css';
+            break;
+        case '.js':
+            contentType = 'text/javascript';
+            break;
+        case '.json':
+            contentType = 'application/json';
+            break;
+        case '.jpg':
+            contentType = 'image/jpeg';
+            break;
+        case '.png':
+            contentType = 'image/png';
+            break;
+        case '.txt':
+            contentType = 'text/plain';
+            break;
+        default:
+            contentType = 'text/html';
+    }
+
+    console.log(`Content Type: ${contentType}`);
+
+    let filePath =
+        contentType === 'text/html' && req.url === '/'
+            ? path.join(__dirname, 'views', 'index.html')
+            : contentType === 'text/html' && req.url.slice(-1) === '/'
+                ? path.join(__dirname, 'views', req.url, 'index.html')
+                : contentType === 'text/html'
+                    ? path.join(__dirname, 'views', req.url)
+                    : path.join(__dirname, req.url);
+    
+    // makes the .html extension not required in the browser
+    if (!extension && req.url.slice(-1) !== '/') filePath += '.html';
+
+
+    const fileExists = fs.existsSync(filePath);
+
+    if (fileExists) {
+        serveFile(filePath,contentType,res);
+    } else {
+        
+        switch(path.parse(filePath).base){
+            case 'old-page.html':
+                res.writeHead(301,{'location': '/new-page.html'});
+                res.end();
+                break;
+            case 'www-page.html':
+                res.writeHead(301,{'location': '/'});
+                res.end();
+                break;
+            default:
+                serveFile(path.join(__dirname,'views','404.html'),'text/html',res);
+        }
+        
+    }
+
     
 });
 
